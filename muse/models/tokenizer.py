fafeda960f89655faf709417d6d95f305b9066d2
Louis Bradshaw <Louisb19@live.co.uk>
2023-04-02 20:08:56 +0100

    refactor tokenizer

--- muse/models/tokenizer.py
+++ muse/models/tokenizer.py
@@ -50,41 +50,12 @@ class Tokenizer:
         model_config.pad_id = self.tok_to_id[self.pad_tok]
         model_config.mask_id = self.tok_to_id[self.mask_tok]
 
-    def seq(self):
-        pass
-
-    def encode(self):
-        pass
-
-    def decode(self):
-        pass
-
-    def apply(self):
-        pass
-
-
-class PretrainTokenizer(Tokenizer):
-    """Tokenizes and sequentialises PianoRoll objects for pre-training.
-
-    Args:
-        model_config (model.ModelConfig): Config for model.
-        device (str): Device to sent PyTorch tensors to send torch.tensors to.
-        note_off_rate (float): Rate to randomly add masked off-notes.
-    """
-
-    def __init__(
-        self,
-        model_config: ModelConfig,
-        return_tensors: bool = True,
-        note_off_rate: float = 0.05,
-    ):
-        super().__init__(model_config, return_tensors)
-
-        self.note_off_rate = note_off_rate
-
     # TODO: Remove starting silence.
     def seq(self, piano_roll: PianoRoll):
         """Sequentialise a PianoRoll object into a list (for pre-training).
+
+        If you require different functionality (say, for harmonization)
+        please override this function in the child class.
 
         Args:
             piano_roll (PianoRoll): piano-roll to be made sequential.
@@ -170,6 +141,29 @@ class PretrainTokenizer(Tokenizer):
             seq_dec = [dec_fn(idx) for idx in seq]
 
         return seq_dec
+
+    def apply(self):
+        pass
+
+
+class PretrainTokenizer(Tokenizer):
+    """Tokenizes and sequentialises PianoRoll objects for pre-training.
+
+    Args:
+        model_config (model.ModelConfig): Config for model.
+        device (str): Device to sent PyTorch tensors to send torch.tensors to.
+        note_off_rate (float): Rate to randomly add masked off-notes.
+    """
+
+    def __init__(
+        self,
+        model_config: ModelConfig,
+        return_tensors: bool = True,
+        note_off_rate: float = 0.05,
+    ):
+        super().__init__(model_config, return_tensors)
+
+        self.note_off_rate = note_off_rate
 
     def apply(
         self,
@@ -252,95 +246,6 @@ class FinetuneTokenizer(Tokenizer):
 
         self.note_off_rate = note_off_rate
         self.dist = torch.distributions.beta.Beta(2, 6)
-
-    # TODO: Remove starting silence.
-    def seq(self, piano_roll: PianoRoll):
-        """Sequentialise a PianoRoll object into a list (for pre-training).
-
-        Args:
-            piano_roll (PianoRoll): piano-roll to be made sequential.
-
-        Returns:
-            list[list]: A collection of lists of sequentialised piano-roll.
-        """
-
-        def _get_seq(roll: list, beginning: bool = False):
-            """Sequentialises roll backwards, modifying roll inplace."""
-            # Add bos token if beginning is True
-            if beginning is True:
-                roll.insert(0, [self.bos_tok])
-
-            # Sequentialise up to self.max_seq_len
-            seq = []
-            # -3 counts for a possible off_tok, time_tok, and eos_tok
-            while roll and (len(seq) + len(roll[0]) <= self.max_seq_len - 3):
-                for note in roll.pop(0):  # Modifies roll inplace
-                    seq.append(note)
-
-                # Randomly add note-offs
-                if random.uniform(0, 1) < self.note_off_rate:
-                    seq.append(self.off_tok)
-
-                seq.append(self.time_tok)
-
-            # If end of piano-roll, append end of sequence
-            if not roll:
-                seq.pop()  # Remove last time_tok
-                seq.append(self.eos_tok)
-
-            # Pad to self.max_seq_len
-            seq = seq + [self.pad_tok] * (self.max_seq_len - len(seq))
-
-            return seq
-
-        roll = copy.deepcopy(piano_roll.roll)
-        sequences = [_get_seq(roll, beginning=True)]
-
-        while roll:
-            sequences.append(_get_seq(roll, beginning=False))
-
-        return sequences
-
-    def encode(self, seq: list):
-        """Encodes a sequentialised piano-roll.
-
-        Args:
-            seq (list[int|str]): Piano-roll sequence to be encoded.
-
-        Returns:
-            torch.Tensor/list: Encoded sequence. Return type dependent on
-            self.return_tensors.
-        """
-
-        def enc_fn(tok):
-            return self.tok_to_id.get(tok, self.tok_to_id[self.unk_tok])
-
-        if self.return_tensors is True:
-            seq_enc = torch.tensor([enc_fn(tok) for tok in seq])
-        else:
-            seq_enc = [enc_fn(tok) for tok in seq]
-
-        return seq_enc
-
-    def decode(self, seq: Union[torch.Tensor, list]):
-        """Decodes a torch.Tensor/list.
-
-        Args:
-            seq (Union[torch.Tensor, list]): Sequences to be decoded.
-
-        Returns:
-            list: Decoded sequence as a list.
-        """
-
-        def dec_fn(id):
-            return self.id_to_tok.get(id, self.unk_tok)
-
-        if isinstance(seq, torch.Tensor):
-            seq_dec = [dec_fn(idx) for idx in seq.tolist()]
-        else:
-            seq_dec = [dec_fn(idx) for idx in seq]
-
-        return seq_dec
 
     def apply(
         self,
